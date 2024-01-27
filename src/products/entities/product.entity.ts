55175b3a570daa7e858750683c7b6a37d435b603
Jose Alonso <mail@josealonso.dev>
2024-01-27 02:46:08 -0600

    Sync to database the new entity Product

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,12 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  title: string;
+}
