bd8ae55fd7d826bd38e9918e90769b8ad1b409ba
Jose Alonso <mail@josealonso.dev>
2024-01-28 02:50:04 -0600

    Create new field: TAGS

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -34,6 +34,12 @@ export class Product {
   @Column('text')
   gender: string;
 
+  @Column('text', {
+    array: true,
+    default: [],
+  })
+  tags: string[];
+
   @BeforeInsert()
   checkSlugInsert() {
     if (!this.slug) {
