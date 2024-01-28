a2a7c05aa2e03ba072c53fbaa1536d6716b597f5
Jose Alonso <mail@josealonso.dev>
2024-01-28 01:09:22 -0600

    Create a script before insert or update
    to transform the data or complement

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,10 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Product {
@@ -27,4 +33,16 @@ export class Product {
 
   @Column('text')
   gender: string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  checkSlugInsert() {
+    if (!this.slug) {
+      this.slug = this.title;
+    }
+    this.slug = this.slug
+      .toLocaleLowerCase()
+      .replaceAll(' ', '_')
+      .replaceAll("'", '');
+  }
 }
