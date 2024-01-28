8922dbc3a42f25db76cfb941e9b1f93789f46b35
Jose Alonso <mail@josealonso.dev>
2024-01-28 01:32:47 -0600

    Create Methods, List, GetOne, Delete

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,6 +3,7 @@ import {
   Injectable,
   InternalServerErrorException,
   Logger,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -29,6 +30,35 @@ export class ProductsService {
     }
   }
 
+  async findAll() {
+    return await this.productRepository.find();
+  }
+
+  async findOne(id: string) {
+    const product = await this.productRepository.findOne({ where: { id: id } });
+
+    if (!product) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+
+    return product;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  async remove(id: string) {
+    // await this.productRepository
+    //   .createQueryBuilder()
+    //   .delete()
+    //   .from(Product)
+    //   .where('id = :id', { id: id })
+    //   .execute();
+    const product = await this.findOne(id);
+    await this.productRepository.remove(product);
+  }
+
   private handleExceptions(error: any) {
     if (error.code === '23505') {
       throw new BadRequestException(error.detail);
@@ -37,21 +67,5 @@ export class ProductsService {
     throw new InternalServerErrorException(
       'Unexpected error, check server logs!',
     );
-  }
-
-  findAll() {
-    return `This action returns all products`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
-  }
-
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} product`;
   }
 }
