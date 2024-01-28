7508773eec4ee730acdd1709891f5ef40650ccb4
Jose Alonso <mail@josealonso.dev>
2024-01-28 00:41:19 -0600

    Method POST with TypeORM
    using the TypeORM repository

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto);
+      const productSaved = await this.productRepository.save(product);
+
+      return productSaved;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException('Help!');
+    }
   }
 
   findAll() {
