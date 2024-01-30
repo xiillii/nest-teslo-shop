9700e1c7280e68f7a5e7d9bb6ed7f83d0eacedc5
Jose Alonso <mail@josealonso.dev>
2024-01-29 22:00:09 -0600

    Implement Passport authentication. Install packages
    yarn add @nestjs/passport passport
    yarn add @nestjs/jwt passport-jwt
    yarn add -D @types/passport-jwt

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,11 +3,20 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product, ProductImage } from './entities';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
-  imports: [TypeOrmModule.forFeature([Product, ProductImage])],
+  imports: [
+    TypeOrmModule.forFeature([Product, ProductImage]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    // JwtModule.register({
+    //   secret: process.env.SECRET,
+    //   signOptions: { expiresIn: '2h' },
+    // }),
+  ],
   exports: [ProductsService],
 })
 export class ProductsModule {}
