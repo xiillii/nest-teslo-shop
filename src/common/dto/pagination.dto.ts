c3576408c5f673edaa2078711deb718f89fc8f29
Jose Alonso <mail@josealonso.dev>
2024-02-01 18:50:18 -0600

    Swagger Document. Body dtos and Query dtos

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,12 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsOptional, IsPositive } from 'class-validator';
 
 export class PaginationDto {
+  @ApiProperty({ default: 10, description: 'How many rows do you need?' })
   @IsOptional()
   @IsPositive()
   @Type(() => Number)
   limit?: number;
 
+  @ApiProperty({
+    default: 0,
+    description: 'How many rows  do you need to skip',
+  })
   @IsOptional()
   @Type(() => Number)
   offset?: number;
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsArray,
   IsIn,
@@ -10,40 +11,55 @@ import {
 } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'Product Title',
+    uniqueItems: true,
+    nullable: false,
+    minLength: 1,
+    example: 'T-Shirt teslo',
+  })
   @IsString()
   @MinLength(1)
   title: string;
 
+  @ApiProperty()
   @IsNumber()
   @IsPositive()
   @IsOptional()
   price?: number;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   slug?: string;
 
+  @ApiProperty()
   @IsInt()
   @IsPositive()
   @IsOptional()
   stock?: number;
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsArray()
   sizes: string[];
 
+  @ApiProperty()
   @IsIn(['men', 'women', 'kid', 'unisex'])
   gender: string;
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsOptional()
   @IsArray()
   tags: string[];
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsOptional()
   @IsArray()
