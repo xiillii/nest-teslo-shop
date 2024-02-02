364dccf8125393ec2ce15e611398607f49b70a39
Jose Alonso <mail@josealonso.dev>
2024-02-01 20:33:41 -0600

    WebSockets: Listen Connect and Disconnect
    yarn add socket.io

--- src/message-web-socket/message-web-socket.gateway.ts
+++ src/message-web-socket/message-web-socket.gateway.ts
@@ -1,9 +1,24 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
 import { MessageWebSocketService } from './message-web-socket.service';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
-export class MessageWebSocketGateway {
+export class MessageWebSocketGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   constructor(
     private readonly messageWebSocketService: MessageWebSocketService,
   ) {}
+
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log(`Client connected${client.id} with arguments ${args}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconected ${client.id}`);
+  }
 }
